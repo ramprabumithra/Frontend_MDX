1716fe6280ce80b072ffb456788170a984c083db
ramprabumithra <92565142+ramprabumithra@users.noreply.github.com>
2024-12-06 00:47:23 +0400

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Lessons</h1>
+    <ul>
+      <li v-for="lesson in lessons" :key="lesson._id">{{ lesson.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import apiClient from './axiosConfig';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      lessons: [],
+    };
+  },
+  methods: {
+    async fetchLessons() {
+      try {
+        const response = await apiClient.get('/collections/Lessons');
+        this.lessons = response.data;
+      } catch (error) {
+        console.error('Error fetching lessons:', error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchLessons();
+  },
+};
+</script>
+
+<style scoped>
+</style>
